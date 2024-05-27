b1d5acf8fcf9bb7efeece169c46707b02de8924c
Nixvip <152063963+9cf@users.noreply.github.com>
2024-05-27 20:35:07 +0700

    1.6

--- source/js/nixvip.js
+++ source/js/nixvip.js
@@ -12,29 +12,31 @@ function disableselect(e) {
     document.onmouseup = reEnable;
   }
 
-  fetch('https://api.mcstatus.io/v2/status/bedrock/SERVER.NIXVIP.TOP')
-    .then(response => response.json())
-    .then(data => handleServerStatus(data))
   
-  function handleServerStatus(data){
-      if(data.status=='error'){
-          console.log(data.error);
-          return false;
-      }
-        
+  function initServerData(serverIp){
+    const serverIpElement = document.getElementById('server-ip');
+    serverIpElement.innerHTML = `<span class="loader"></span>`;
+    fetch('https://api.mcstatus.io/v2/status/bedrock/'+serverIp)
+    .then(response => response.json())
+    .then(data => handleServerStatus(data));
+
+    function handleServerStatus(data){
+        if(data.online=='false'){
+            console.log(data.online);
+            return false;
+        }
+      const versionServer = document.getElementById("version-server");
+      versionServer.innerHTML = `Minecraft Bedrock เวอร์ชัน `+(data.version.name);
+
       const serverIpElement = document.getElementById('server-ip');
       serverIpElement.innerHTML = (data.host);
-
-      const version = document.getElementById("version");
-      version.innerHTML = `Minecraft Bedrock เวอร์ชัน `+(data.version.name);
   
       const playerCounter = document.getElementById("player-count");
       playerCounter.innerHTML = `( ออนไลน์ `+(data.players.online)+` คน )`;
   
       const addServer = document.getElementById("add-server");
       addServer.innerHTML = `<a href="minecraft:?addExternalServer=Nixvip|server.nixvip.top:19132" ><button class="button"><i class="fa fa-plus-circle" aria-hidden="true"></i> เพิ่มเซิฟเวอร์</button></a>`;
+    } 
+}
   
-      const logo = document.getElementById("server-icon");
-      logo.src = (data.favicon);
-  } 
-  
+
