85f957e982702d6269771fb3d16af40d14423493
Nixvip <152063963+9cf@users.noreply.github.com>
2024-05-27 15:10:33 +0700

    1.1

--- source/js/nixvip.js
+++ source/js/nixvip.js
@@ -11,3 +11,33 @@ function disableselect(e) {
     document.onmousedown = disableselect;
     document.onmouseup = reEnable;
   }
+
+  function initServerData(serverIp){
+    const serverIpElement = document.getElementById('server-ip');
+    serverIpElement.innerHTML = (serverIp);
+  
+    console.log('https://api.mcstatus.io/v2/status/bedrock/'+serverIp);
+    fetch('https://api.mcstatus.io/v2/status/bedrock/'+serverIp)
+    .then(response => response.json())
+    .then(data => handleServerStatus(data));
+  
+    function handleServerStatus(data){
+        if(data.status=='error'){
+            console.log(data.error);
+            return false;
+        }
+  
+        const version = document.getElementById("version");
+        version.innerHTML = `Minecraft Bedrock เวอร์ชัน `+(data.version.name);
+  
+        const playerCounter = document.getElementById("player-count");
+        playerCounter.innerHTML = `ออนไลน์ `+(data.players.online)+` คน`;
+  
+        const addServer = document.getElementById("add-server");
+        addServer.innerHTML = `<a href="minecraft:?addExternalServer=Nixvip|server.nixvip.top:19132" ><button class="button">เพิ่มเซิฟเวอร์</button></a>`;
+  
+        const logo = document.getElementById("server-icon");
+        logo.src = (data.favicon);
+    } 
+  
+  }
